9bc10f159e54662eeca00cc935fdc2b3b29d1e63
anonym77ms@gmail.com <developer@linux.suse>
2018-07-30 20:33:06 +0300

    update

--- sw.js
+++ sw.js
@@ -2,9 +2,9 @@
 
 importScripts('sw-toolbox.js');
 
-toolbox.precache(["index.html","assets/*"]);
+toolbox.precache(["index.html","style/style.css"]);
 
-toolbox.router.get('/assets/img/*', toolbox.cacheFirst);
+toolbox.router.get('/images/*', toolbox.cacheFirst);
 
 toolbox.router.get('/*', toolbox.networkFirst, {
   networkTimeoutSeconds: 5
